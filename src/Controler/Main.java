8deb8a69af4120ecef44f67989ebe6a93da76e96
kaetir <thibault.desprez@isen.yncrea.fr>
2019-05-01 19:08:35 +0200

    primary interface

--- src/Controler/Main.java
+++ src/Controler/Main.java
@@ -6,13 +6,16 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+
 public class Main extends Application {
+
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("View/sample.fxml"));
+
+        Parent root = FXMLLoader.load(getClass().getResource("../View/sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 900));
         primaryStage.show();
     }
 
