37a7e7ebb9675f85adf4724dcd51902336618fbc
kaetir <thibault.desprez@isen.yncrea.fr>
2019-05-02 11:47:05 +0200

    ajout des routes

--- src/View/View.java
+++ src/View/View.java
@@ -1,5 +1,7 @@
 package View;
 
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import com.sun.xml.internal.fastinfoset.algorithm.BooleanEncodingAlgorithm;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.*;
@@ -34,12 +36,50 @@ public class View {
 
     }
 
+    public void boutonRoute(){
+
+        gc = Drawing_Canvas.getGraphicsContext2D();
+        gc.setFill(Color.GREEN);
+
+        gc.setLineWidth(4);
+        gc.setStroke(Color.GREEN);
+
+        Drawing_Canvas.setOnMouseClicked(new EventHandler<MouseEvent>() {
+            double x,y;
+            Boolean firstPointReady = false;
+
+            @Override
+            public void handle(MouseEvent mouseEvent) {
+                if (!firstPointReady){
+                    x = mouseEvent.getX();
+                    y = mouseEvent.getY();
+                    firstPointReady = true;
+
+                }else{
+                    DrawRoute(x, y, mouseEvent.getX(), mouseEvent.getY());
+                    firstPointReady = false;
+                }
+            }
+        });
+
+    }
+
     @FXML
     public void DrawVille(double x, double y){
 
         gc = Drawing_Canvas.getGraphicsContext2D();
         gc.fillRect(x,y,50,50);
     }
+
+
+    @FXML
+    public void DrawRoute(double x, double y, double x1, double y1){
+
+        gc = Drawing_Canvas.getGraphicsContext2D();
+        gc.strokeLine(x,y,x1,y1);
+    }
+
+
 
     public View(){
 
