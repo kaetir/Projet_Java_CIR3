effd2cc616e03fac60d78bb0100df0122ac43989
kaetir <thibault.desprez@isen.yncrea.fr>
2019-05-06 09:37:39 +0200

    ajout du display des intersection

---
+++ src/View/Displayable/DisplayIntersection.java
@@ -0,0 +1,12 @@
+package View.Displayable;
+
+public class DisplayIntersection implements Displayable {
+    private double x;
+    private double y;
+    private int nbJonction;
+
+    @Override
+    public void Draw() {
+
+    }
+}
