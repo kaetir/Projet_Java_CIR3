6f9d71bca56ede74be15afe6eaf40a1b6efff9bc
Unknown <toninvalet@gmail.com>
2019-05-17 15:44:28 +0200

    Adding code to DisplayCity and DisplayRoad obj

--- src/View/Displayable/DisplayCity.java
+++ src/View/Displayable/DisplayCity.java
@@ -1,4 +1,33 @@
 package View.Displayable;
 
+import java.util.Vector;
+
 public class DisplayCity {
+    private double x,y;
+    private int id;
+    private Vector<DisplayVehicle> vehicles;
+
+    public DisplayCity(double x, double y, int id, Vector<DisplayVehicle> vehicles){
+        this.x = x;
+        this.y = y;
+        this.id = id;
+        this.vehicles = vehicles;
+    }
+
+    public double get_X(){
+        return this.x;
+    }
+
+    public double get_Y(){
+        return this.y;
+    }
+
+    public int get_ID(){
+        return this.id;
+    }
+
+    public Vector<DisplayVehicle> get_Vehicles(){
+        return this.vehicles;
+    }
+
 }
--- src/View/Displayable/DisplayRoad.java
+++ src/View/Displayable/DisplayRoad.java
@@ -1,4 +1,25 @@
 package View.Displayable;
 
 public class DisplayRoad {
+    private boolean multi;
+    private int lanes;
+    private int id;
+
+    public DisplayRoad(int id, int lanes, boolean multi){
+        this.id = id;
+        this. lanes = lanes;
+        this.multi = multi;
+    }
+
+    public int get_ID(){
+        return this.id;
+    }
+
+    public int get_Lanes(){
+        return this.lanes;
+    }
+
+    public boolean get_Multi(){
+        return this.multi;
+    }
 }
