a35fb3cbefec3d1fea72559cd9f7e4a1b4b91724
kaetir <thibault.desprez@isen.yncrea.fr>
2019-05-18 18:50:04 +0200

    je vais manger des frites

--- src/Controler/Main.java
+++ src/Controler/Main.java
@@ -20,7 +20,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
-        Parent root = FXMLLoader.load(getClass().getResource("../View/sample.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("../View/main.fxml"));
         primaryStage.setTitle("City Road");
         primaryStage.setScene(new Scene(root, 1280, 900));
         primaryStage.show();
--- src/View/Displayable/DisplayVehicle.java
+++ src/View/Displayable/DisplayVehicle.java
@@ -1,13 +1,39 @@
 package View.Displayable;
 
+import javafx.scene.paint.Color;
+
+import static View.View.gc;
+
 public class DisplayVehicle implements Displayable {
 
-    private double x,y;
-    private enum type{ car, truck , motorBike  };
+    private double x,y, lastx, lasty;
+    private enum type{ car, truck , motorBike;};
+    private type vasistas; // what is that allemand
 
-    public DisplayVehicle(double x, double y) {
+
+    //constructor
+    public DisplayVehicle(double x, double y, double lastx, double lasty, type vasistas) {
         this.x = x;
         this.y = y;
+        this.lastx = lastx;
+        this.lasty = lasty;
+        this.vasistas = vasistas;
+    }
+
+    public double getLastx() {
+        return lastx;
+    }
+
+    public void setLastx(double lastx) {
+        this.lastx = lastx;
+    }
+
+    public double getLasty() {
+        return lasty;
+    }
+
+    public void setLasty(double lasty) {
+        this.lasty = lasty;
     }
 
     public double getX() {
@@ -26,12 +52,49 @@ public class DisplayVehicle implements Displayable {
         this.y = y;
     }
 
-    @Override
-    public boolean isIn(double x, double y) {
-        return false;
+    public type getVasistas() {
+        return vasistas;
     }
 
+    public void setVasistas(type vasistas) {
+        this.vasistas = vasistas;
+    }
+
+    //get colision between a point and a Vehicule
+    @Override
+    public boolean isIn(double x, double y) {
+        double xmin = (this.lastx < this.x )? lastx : this.x ;
+        double xmax = (this.lastx > this.x )? lastx : this.x ;
+        double ymin = (this.lasty > this.y )? lasty : this.y ;
+        double ymax = (this.lasty > this.y )? lasty : this.y ;
+        return (x > xmin && x < xmax && y > ymin && y < ymax);
+    }
+
+
+    // display a vehicule on the canvas
     public void Draw(){
+        switch (vasistas){
+            case car:
+                gc.setFill(Color.BISQUE);
+                gc.setStroke(Color.BISQUE);
+                gc.setLineWidth(1);
+
+                break;
+
+            case truck:
+                gc.setFill(Color.PAPAYAWHIP);
+                gc.setStroke(Color.PAPAYAWHIP);
+                gc.setLineWidth(2);
+
+                break;
+
+            case motorBike:
+                gc.setFill(Color.CHOCOLATE);
+                gc.setStroke(Color.CHOCOLATE);
+                gc.setLineWidth(3);
+
+                break;
+        }
 
 
     };
