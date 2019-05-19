07fd66df2020a3a76d552b3073b6e2b9e5571dce
Unknown <matthieularosasimon@gmail.com>
2019-05-19 02:03:44 +0200

    Commencement de la simulation

--- src/Model/Vehicules/Vehicule.java
+++ src/Model/Vehicules/Vehicule.java
@@ -2,26 +2,41 @@ package Model.Vehicules;
 
 abstract public class Vehicule {
 
-    public Vehicule.type name;
-    protected final double maxVitesse;
+    //Enumération du type de véhicule
+    public enum type {car, truck , motorBike}
+
+    //Paramètres d'un véhicule
+    protected static Vehicule.type type;
+    protected final double maxSpeed;
     protected final double size;
     protected final boolean canPass;
     protected double x;
     protected double y;
     protected double oldX;
     protected double oldY;
-    public enum type {car, truck , motorBike};
 
+    //Construteur
     public Vehicule(double maxVitesse, double sizeMin, double sizeMax, boolean canPass, Vehicule.type name) {
-        this.maxVitesse = maxVitesse;
+        this.maxSpeed = maxVitesse;
         this.size = randSize(sizeMin, sizeMax);
         this.canPass = canPass;
-        this.name = name;
+        this.type = name;
         System.out.println(name + " created");
     }
 
+    //Choix aléatoire de la taille du véhicule comprise entre sizeMin et sizeMax
     public double randSize(double sizeMin, double sizeMax){
         return (sizeMin + (Math.random() * ((sizeMax - sizeMin) + 1)));
+    }
+
+    //Affichage du type de véhicule et de ses coordonnées
+    public void print(){
+        System.out.println("    " + this.type + " (" + this.x + ", " + this.y + ")");
+    }
+
+    //Getters et Setters
+    public type getType() {
+        return type;
     }
 
     public double getX() {
@@ -56,8 +71,8 @@ abstract public class Vehicule {
         this.oldY = oldY;
     }
 
-    public double getMaxVitesse() {
-        return maxVitesse;
+    public double getMaxSpeed() {
+        return maxSpeed;
     }
 
     public double getSize() {
