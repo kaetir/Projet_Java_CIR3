9a6f58a3ceeaf23ca3a906adc70f51fad4dd70b7
Unknown <matthieularosasimon@gmail.com>
2019-05-21 23:28:36 +0200

    Et on vire une erreur de véhicule

--- src/Model/Vehicules/VehiculeFactory.java
+++ src/Model/Vehicules/VehiculeFactory.java
@@ -1,29 +1,21 @@
 package Model.Vehicules;
 
 import Model.Model;
-import Model.Vehicules.Exception.VehiculeCreationException;
 
 public class VehiculeFactory {
 
     //Fonction de création des véhicules
-    public static Vehicule create(Vehicule.type vehType, int destination) throws VehiculeCreationException {
+    public static Vehicule create(Vehicule.type vehType, int destination) {
 
-        try {
+        //Création du véhicule en fonction du paramètre fourni
+        if(Vehicule.type.car.equals(vehType))               //Création d'une voiture
+                return new Car(Model.getCities().get(destination));
+        else if(Vehicule.type.motorBike.equals(vehType))    //Création d'une moto
+                return new MotorBike(Model.getCities().get(destination));
+        else if(Vehicule.type.truck.equals(vehType))        //Création d'un camion
+                return new Truck(Model.getCities().get(destination));
 
-            //Création du véhicule en fonction du paramètre fourni
-            if(Vehicule.type.car.equals(vehType))               //Création d'une voiture
-                    return new Car(Model.getCities().get(destination));
-            else if(Vehicule.type.motorBike.equals(vehType))    //Création d'une moto
-                    return new MotorBike(Model.getCities().get(destination));
-            else if(Vehicule.type.truck.equals(vehType))        //Création d'un camion
-                    return new Truck(Model.getCities().get(destination));
-
-            //Renvoi d'une erreur
-            throw new VehiculeCreationException(vehType);
-
-        } catch (VehiculeCreationException e){
-            return null;
-        }
+        return null;
 
     }
 
