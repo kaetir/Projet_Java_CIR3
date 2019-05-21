ea75e85ec6658798d25de12a03a50f6f8c2dfb95
Unknown <matthieularosasimon@gmail.com>
2019-05-21 22:32:48 +0200

    Destinatien again

--- src/Model/Vehicules/VehiculeFactory.java
+++ src/Model/Vehicules/VehiculeFactory.java
@@ -1,22 +1,22 @@
 package Model.Vehicules;
 
-import Model.City;
+import Model.Model;
 import Model.Vehicules.Exception.VehiculeCreationException;
 
 public class VehiculeFactory {
 
     //Fonction de création des véhicules
-    public static Vehicule create(Vehicule.type vehType, City destination) throws VehiculeCreationException {
+    public static Vehicule create(Vehicule.type vehType, int destination) throws VehiculeCreationException {
 
         try {
 
             //Création du véhicule en fonction du paramètre fourni
             if(Vehicule.type.car.equals(vehType))               //Création d'une voiture
-                    return new Car(destination);
+                    return new Car(Model.getCities().get(destination));
             else if(Vehicule.type.motorBike.equals(vehType))    //Création d'une moto
-                    return new MotorBike(destination);
+                    return new MotorBike(Model.getCities().get(destination));
             else if(Vehicule.type.truck.equals(vehType))        //Création d'un camion
-                    return new Truck(destination);
+                    return new Truck(Model.getCities().get(destination));
 
             //Renvoi d'une erreur
             throw new VehiculeCreationException(vehType);
