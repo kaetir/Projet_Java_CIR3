372248f81d1ede9eadcdfacef619ae611d594f4f
kaetir <thibault.desprez@isen.yncrea.fr>
2019-05-25 01:07:00 +0200

    patch des intersections

--- src/Controler/Controller.java
+++ src/Controler/Controller.java
@@ -11,6 +11,7 @@ import javafx.util.Pair;
 import Model.Simulation;
 
 import java.io.Console;
+import java.util.NoSuchElementException;
 import java.util.Vector;
 
 public class Controller {
@@ -50,86 +51,17 @@ public class Controller {
         Model.createCity(city.getId(), city.getX(), city.getY());
     }
 
+
     //Create a new road on the grid
     public void createRoad(DisplayRoad road, int id1, int id2) throws RoadCreationException{
 
         Model.createRoad(road.getNbVoies(), Model.getCity(id1) , Model.getCity(id2), road.getDots());
 
-        Vector<DisplayRoad> roads = view.getDisplayRoads();
-        Vector<Pair<Double, Double>> dots = road.getDots();
-        double x1, y1, x1_1, y1_1;
-        double x2, y2, x2_1, y2_1;
-        double a1, b1, c1, a2, b2,c2;
-        for(int i = 0; i < roads.size(); i++){
-            // pas la meme route
-            if(roads.elementAt(i).equals(road) == false ){
-
-                // parcourt de la route
-                for(int k = 0; k < dots.size()-1; k++){
-                    // route actuelle point 1
-                    x1 = dots.elementAt(k).getKey();
-                    y1 = dots.elementAt(k).getValue();
-                    // route actuelle point 2
-                    x1_1 = dots.elementAt(k+1).getKey();
-                    y1_1 = dots.elementAt(k+1).getKey();
-
-                    // parcours des autres routes
-                    for(int n = 0; n < roads.elementAt(i).getDots().size()-1; n++ ){
-                        // point 1
-                        x2 = roads.elementAt(i).getDots().elementAt(n).getKey();
-                        y2 = roads.elementAt(i).getDots().elementAt(n).getValue();
-                        // point 2
-                        x2_1 = roads.elementAt(i).getDots().elementAt(n+1).getKey();
-                        y2_1 = roads.elementAt(i).getDots().elementAt(n+1).getValue();
-
-                        a1 = y1_1 - y1;   //a1 = B.y - A.y
-                        b1 = x1 - x1_1;   //b1 = A.x - B.x
-                        c1 = a1*(x1)+ b1*(y1);
-
-                        a2 = y2_1 - y2;
-                        b2 = x2 - x2_1;
-                        c2 = a2*(x2)+ b2*(y2);
-
-                        double determinant = a1*b2 - a2*b1;
-                        if(determinant != 0){
-                            double x = (-1/((a1*b2)-(a2*b1)))*((b2*c1)-(b1*c2));
-                            double y = (-1/((a1*b2)-(a2*b1)))*((-a2*c1)+(a1*c2));
-
-                            if(k == 0 && n == 0){
-                                if( !(dots.elementAt(k).equals(roads.elementAt(i).getDots().elementAt(n))) ){
-                                    view.addIntersection(Math.abs(x),Math.abs(y));
-                                    view.refresh();
-                                }
-                            }else if(k == 0 && n == roads.elementAt(i).getDots().size()-2){
-                                if( !(dots.elementAt(k).equals(roads.elementAt(i).getDots().elementAt(n+1))) ){
-                                    view.addIntersection(Math.abs(x),Math.abs(y));
-                                    view.refresh();
-                                }
-                            }else if(k == dots.size()-2 && n == roads.elementAt(i).getDots().size()-2){
-                                if( !(dots.elementAt(k+1).equals(roads.elementAt(i).getDots().elementAt(n+1))) ){
-                                    view.addIntersection(Math.abs(x),Math.abs(y));
-                                    view.refresh();
-                                }
-                            }else if(k == dots.size()-2 && n == 0){
-                                if( !(dots.elementAt(k+1).equals(roads.elementAt(i).getDots().elementAt(n))) ){
-                                    view.addIntersection(Math.abs(x),Math.abs(y));
-                                    view.refresh();
-                                }
-                            }else{
-                                view.addIntersection(Math.abs(x),Math.abs(y));
-                                view.refresh();
-                            }
-                        }
-
-
-                    }
-
-
-                }
-
-
-            }
-
+        for (DisplayRoad dr: view.getDisplayRoads() ) {
+            Vector<Pair<Double, Double>> v = road.colide(dr);
+            v.forEach(pos -> {if(view.colideCity(pos.getKey(), pos.getValue()) == null)
+                                view.addIntersection(pos.getKey(), pos.getValue());
+                             });
         }
     }
 
